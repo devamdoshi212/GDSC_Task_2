ef563bf9f7c552402e6fb252aee3fd590bf1706f
Devam Doshi <devamdoshi212@gmail.com>
2023-01-01 18:45:34 +0530

    Create Palidrome_String.cpp

---
+++ Palidrome_String.cpp
@@ -0,0 +1,56 @@
+// https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    int n=S.length(),c=1;
+	    for(int i=0;i<n/2;i++)
+	    {
+	        if(S[i]!=S[n-i-1])
+	        {
+	            c=0;
+	            break;
+	        }
+	    }
+	    if(c==0)
+	    return 0;
+	    else
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
